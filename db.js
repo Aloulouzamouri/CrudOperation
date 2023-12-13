aee03e75051e88f25832738257a28dcd637e23bd
Alaa Eddine zamouri <91261540+Aloulouzamouri@users.noreply.github.com>
2023-12-13 13:44:08 +0100

    Add files via upload

---
+++ db.js
@@ -0,0 +1,8 @@
+//fichier contenant les informations confidentielles de la base de donnees
+
+const mongoose = require('mongoose')
+const dbUri = 'mongodb+srv://admin:1234@cluster0.ptfku0u.mongodb.net/employee_db?retryWrites=true&w=majority'
+module.exports = () => {
+    return mongoose.connect(dbUri)
+
+}
